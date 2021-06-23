2b234d102dc6a86a48d83757a978359cd212c2ed
bidaffonso <bidaffondo@gmail.com>
2021-06-23 19:36:58 -0400

    Final commit with errors

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
 import React, {useContext, useEffect} from 'react';
+import Posts from './components/Posts';
+import Selector from './components/Selector';
+import LastUpdate from './components/LastUpdate';
+import RefreshButton from './components/RefreshButton';
+import { Context } from './components/RedditContext';
+
+function App() {
+  const { fetchPosts, isFetching, postsBySubreddit, selectedSubreddit } = useContext(Context);
+
+  useEffect(() => {
+    fetchPosts();
+  },);
+
+  const { items: posts = [] } = postsBySubreddit[selectedSubreddit];
+  const isEmpty = posts.legth === 0;
+
+  return (
+    <div>
+      <Selector />
+      <div>
+        <LastUpdate />
+        <RefreshButton />
+      </div>
+      {isFetching && <h2>Loading...</h2>}
+      {!isFetching && isEmpty && <h2>Empty.</h2>}
+      {!isFetching && !isEmpty && <Posts />}
+    </div>
+  );
+}
+
+export default App;
